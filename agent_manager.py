4c2cb4686a8130268a3cce5e20af677f4803fdec
Gabriel Wendell <67518461+GabrielWendell@users.noreply.github.com>
2025-10-31 15:17:59 -0300

    Create agent_manager.py

---
+++ agent_manager.py
@@ -0,0 +1,7 @@
+from langgraph.checkpoint.memory import MemorySaver
+from ema.core.s3_agent_manager import AgentModel
+from ema.core.base_agents import BaseAgent
+from ema.core.chat_iara import ChatIaraGenIA
+from ema.core.memory import memory
+from typing import List
+
