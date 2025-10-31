3d3744e8d7ff0d8351abf21c725773b3586c9c95
Gabriel Wendell <67518461+GabrielWendell@users.noreply.github.com>
2025-10-31 15:20:14 -0300

    Update agent_manager.py

--- agent_manager.py
+++ agent_manager.py
@@ -5,3 +5,14 @@ from ema.core.chat_iara import ChatIaraGenIA
 from ema.core.memory import memory
 from typing import List
 
+def create_agent(agent: AgentModel, memory: MemorySaver = None):
+  llm = ChatIaraGenIA(agent["model"], agent["temperature"])
+
+  return BaseAgent(
+        model = llm,
+        tools = agent["tools"],
+        name = agent["slug"],
+        description = agent["description"],
+        checkpointer = memory,
+        prompt = agent["prompt"]
+      )
